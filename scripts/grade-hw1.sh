3d4745403ebdf9a6e259b6c9ac2fa7626dfd69eb
Minseong Jang <minseong.jang@cp.kaist.ac.kr>
2022-02-28 09:58:42 +0900

    Add grader for h1

---
+++ scripts/grade-hw1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit when any command fails.
+set -e
+
+# Run lints.
+cargo fmt --all -- --check
+cargo clippy
+
+# Run tests.
+RUST_MIN_STACK=33554432 cargo test test_examples_write_c --release
+python3 tests/fuzz.py --print -n30
