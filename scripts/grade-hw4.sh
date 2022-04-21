268bebc91edaa9814f10b5be83a0b574846fdcc4
Minseong Jang <minseong.jang@cp.kaist.ac.kr>
2022-04-21 18:23:53 +0900

    Add grading scripts

---
+++ scripts/grade-hw4.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Exit when any command fails.
+set -e
+
+# Run lints.
+cargo fmt --all -- --check # run `cargo fmt` to auto-correct format.
+cargo clippy
+
+# Run tests.
+RUST_MIN_STACK=33554432 cargo test --release test_examples_mem2reg
