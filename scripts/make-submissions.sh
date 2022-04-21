b2c8838dc83605592df2f4b82a39f12c7ed818f7
Minseong Jang <minseong.jang@cp.kaist.ac.kr>
2022-04-21 17:52:47 +0900

    Add submission script for hw2

--- scripts/make-submissions.sh
+++ scripts/make-submissions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-rm -rf hw4.zip hw5.zip
+# Clears the previous submissions.
+rm -rf hw2.zip hw3.zip hw4.zip hw5.zip hw6.zip
+
+# Creates new submissions.
+zip hw2.zip -j src/c/write_c.rs src/irgen/mod.rs
 zip hw3.zip -j src/opt/opt_utils.rs src/opt/simplify_cfg.rs
 zip hw4.zip -j src/opt/opt_utils.rs src/opt/mem2reg.rs
 zip hw5.zip -j src/opt/opt_utils.rs src/opt/gvn.rs
